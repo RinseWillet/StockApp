acaa5f6a3dbafec139b1eaa5c64cdfc92e76203e
RinseWillet <r.willet@gmail.com>
2021-06-14 17:59:34 +0200

    paar aanpassingen

--- script.js
+++ script.js
@@ -14,15 +14,11 @@
 // input veld voor type aandeel maken
 // crypto currency tracker
 
-<<<<<<< HEAD
-=======
-var dataTradevolume = [];
->>>>>>> 615ecb29c2fb5bbb06312c3a6a7a3c309393d6ef
 var dataHigh = [];
 var dataLow = [];
 var dataLabels = [];
 
-<<<<<<< HEAD
+//als op invoeren gedrukt wordt start aanmaken van grafiek
 document.getElementById('invoeren').addEventListener("click", () => {
     allData();
 });
@@ -74,11 +70,11 @@ const stockDataParsing = async (data) => {
     //data uit de variabele halen voor hoogste ("2. high") en laagste ( "3. low") handelsprijzen en het handelsvolume ("5. volume")
     for (x in tijdsData) {
         tempHigh.push(tijdsData[x]["2. high"]);
-        tempLow.push(tijdsData[x]["3. low"]);       
+        tempLow.push(tijdsData[x]["3. low"]);
     }
 
     // het omdraaien van de volgorde 
-    dataLabels = tempLabels.reverse();    
+    dataLabels = tempLabels.reverse();
     dataHigh = tempHigh.reverse();
     dataLow = tempLow.reverse();
 }
@@ -93,73 +89,13 @@ const setGraphs = (labels, laag, hoog, naam) => {
             backgroundColor: '#004cff',
             borderColor: '#004cff',
             data: laag,
-        },{
+        }, {
             label: naam + " - hoogste prijs",
             backgroundColor: '#ff0000',
             borderColor: '#ff0000',
             data: hoog,
-        }    
-    ]
-=======
-const allData = async () => {
-
-    var dataName = document.getElementById('invoer').value;
-    console.log(dataName);
-    var apiKey = "YOUR_API_KEY";
-
-    //Alpha Vantage data API adres (url)
-    var url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + dataName + "&apikey=" + apiKey;
-    
-    const stockDataCall = await fetch(url);    
-    const stockData = await stockDataCall.json();    
-    if(stockData["Error Message"] === null){
-        console.log(stockData["Error Message"]);
-        document.getElementById('message').innerHTML = "foutje";
-        return;       
-    } 
-    const update = await stockDataParsing(stockData);
-    const plotten = await setGraphs(dataLabels, dataLow, dataName);
-    
-}
-
-const stockDataParsing = async (data) => { 
-    
-        // uit de variabele de specifieke data (Time Series) halen en array maken voor de namen (labels) van ieder datapunt (i.e. datum + uur)
-        let tempLabels = Object.keys(data["Time Series (Daily)"]);
-            
-        //de uurdata uit het antwoord lezen (Time Series (Daily))
-        tijdsData = data["Time Series (Daily)"];
-
-        let x;
-        let tempVolume = [];
-        let tempHigh = [];
-        let tempLow = [];
-
-        //data uit de variabele halen voor hoogste ("2. high") en laagste ( "3. low") handelsprijzen en het handelsvolume ("5. volume")
-        for (x in tijdsData) {
-            tempVolume.push(tijdsData[x]["5. volume"]);
-            tempHigh.push(tijdsData[x]["2. high"]);
-            tempLow.push(tijdsData[x]["3. low"]);
         }
-
-        // het omdraaien van de volgorde 
-        dataLabels = tempLabels.reverse();
-        dataTradevolume = tempVolume.reverse();
-        dataHigh = tempHigh.reverse();
-        dataLow = tempLow.reverse();
-}
-
-// functie om de grafiek te plotten met de labels, data en naam als parameters
-const setGraphs = (labels, antwoordData, name) => {
-    const data = {
-        labels: labels,
-        datasets: [{
-            label: name,
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: antwoordData,
-        }]
->>>>>>> 615ecb29c2fb5bbb06312c3a6a7a3c309393d6ef
+        ]
     };
 
     const config = {
@@ -169,21 +105,15 @@ const setGraphs = (labels, antwoordData, name) => {
     };
 
     // de grafiek in de HTML gaan plaatsen
-<<<<<<< HEAD
     var mijnGrafiek = new Chart(
         document.getElementById('myChart'),
         config
     );
 
+    //grafiek verwijderen als op de knop opnieuw gedrukt wordt
     document.getElementById('opnieuw').addEventListener("click", () => {
         mijnGrafiek.destroy();
     }
     )
 }
-=======
-    var myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-    );
-}
->>>>>>> 615ecb29c2fb5bbb06312c3a6a7a3c309393d6ef
+
